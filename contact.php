e92df08ffdc93552002a652facbc5399ef729201
kowai-onigiri <alexischambers627@gmail.com>
2022-04-23 14:50:11 -0400

    update nav links

--- contact.php
+++ contact.php
@@ -30,9 +30,9 @@ if($_POST["submit"]) {
             <div class="topBar">
                 <nav>
                     <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Projects</a></li>
-                        <li><a href="#">Contact</a></li>
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="projects.html">Projects</a></li>
+                        <li><a href="contact.php">Contact</a></li>
                     </ul>
                 </nav>
             </div>
