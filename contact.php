8f8f7b667c97fe37552ee041cdc8acee2a9685ce
kowai-onigiri <alexischambers627@gmail.com>
2022-04-23 14:46:07 -0400

    test form

--- contact.php
+++ contact.php
@@ -1,7 +1,19 @@
 <?php
-if($_POST["message"]) {
-    mail("your@email.address", "Form to email message", $_POST["message"], "From: an@email.address");
+
+if($_POST["submit"]) {
+    $recipient="alexischambers.dev@gmail.com";
+    $subject="Form to email message";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -34,10 +46,22 @@ if($_POST["message"]) {
                 <img src="">
             </div>
             
-            <form method="post" action="contact.php">
-                <textarea name="message"></textarea>
-                <input type="submit">
-            </form>
+            <?=$thankYou ?>
+
+    <form method="post" action="contact.php">
+        <label>Name:</label>
+        <input name="sender">
+
+        <label>Email address:</label>
+        <input name="senderEmail">
+
+        <label>Message:</label>
+        <textarea rows="5" cols="20" name="message"></textarea>
+
+        <input type="submit" name="submit">
+    </form>
+
+</body>
             
         </div>
         <div class="footer">
